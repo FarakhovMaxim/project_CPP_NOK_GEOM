0e681e438398c3147b8ddc5e0709c55a596600ef
FarakhovMaxim <82201471+FarakhovMaxim@users.noreply.github.com>
2024-03-29 10:22:47 +0300

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <fstream>
+#include "nok.cpp"
+#include "geom.cpp"
 using namespace std;
 
 int main() {
-    ifstream file;
     int i;
     cout << "Добро пожаловать!" << endl;
     cout << "Введите 0, если хотите запустить НОК" << endl;
     cout << "Введите 1, если хотите запустить ГЕОМЕТРИЧЕСКАЯ ПРОГРЕССИЯ" << endl;
     if (i == 0) {
-        file.open("nok.cpp");
+        nok_main();
     } else if (i == 1) {
-        file.open("geom.cpp");
+        geom();
     }
 }
